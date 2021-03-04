58c825ccf20f4f8492c55a7049aa55270e460551
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-04 16:01:28 +0100

    compara fechas y pone color correspondiente y dias

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 const urlApi = "http://localhost:3001/facturas";
 const tBody = document.querySelector("tbody");
+const tFoot = document.querySelector("tfoot");
 
 const clonarFila = () => {
   const molde = document.querySelector(".clonada").cloneNode(true);
@@ -7,21 +9,57 @@ const clonarFila = () => {
   return molde;
 };
 
+const estaAbonada = (dato) => dato === true;
+
+// eslint-disable-next-line radix
+const convertirTimestampFecha = (fechaTimestamp) => new Date(parseInt(fechaTimestamp));
+
+const compararFechas = (fecha, fechaVencimiento, vence, dato) => {
+  const DateTimeFecha = luxon.DateTime.fromISO(new Date(parseInt(dato.fecha)).toISOString());
+  const DateTimeFechaVencimiento = luxon.DateTime.fromISO(new Date(parseInt(dato.vencimiento)).toISOString());
+  console.log(fecha.toLocaleDateString(), fechaVencimiento.toLocaleDateString());
+  const restanteDias = Math.trunc(DateTimeFechaVencimiento.diff(DateTimeFecha, "days").values.days);
+  if (fecha > fechaVencimiento) {
+    vence.classList.add("table-danger");
+    vence.textContent = `${fechaVencimiento.toLocaleDateString()} (hace ${restanteDias} dias)`;
+  } else if (fecha <= fechaVencimiento) {
+    vence.classList.add("table-success");
+    vence.textContent = `${fechaVencimiento.toLocaleDateString()} (faltan ${restanteDias} dias)`;
+  }
+};
+
 const rellenarTabla = (dato, numRow, fechaRow, conceptoRow, baseRow, ivaRow, totalRow, estadoRow, venceRow) => {
   numRow.textContent = dato.numero;
   fechaRow.textContent = dato.fecha;
   conceptoRow.textContent = dato.concepto;
-  baseRow.textContent = dato.base;
-  ivaRow.textContent = `${(dato.base * dato.tipoIva) / 100}€ (${dato.tipoIva}%)`;
-  totalRow.textContent = `${(((dato.base * dato.tipoIva) / 100) + dato.base).toFixed(2)}€`;
-  estadoRow.textContent = dato.abonada;
-  venceRow.textContent = dato.vencimiento;
+  baseRow.textContent = `${dato.base}€`;
+  ivaRow.textContent = `${(dato.base * dato.tipoIva) / 100} € (${dato.tipoIva}%)`;
+  totalRow.textContent = `${(((dato.base * dato.tipoIva) / 100) + dato.base).toFixed(2)} €`;
+  const fecha = convertirTimestampFecha(dato.fecha);
+  const fechaDeVencimiento = convertirTimestampFecha(dato.vencimiento);
+  // console.log(fechaSinTimestamp);
+  if (estaAbonada(dato.abonada)) {
+    estadoRow.classList.add("table-success");
+    estadoRow.textContent = "Abonada";
+    venceRow.classList.add("table-success");
+    venceRow.textContent = "-";
+  } else {
+    estadoRow.classList.add("table-danger");
+    estadoRow.textContent = "Pendiente";
+    compararFechas(fecha, fechaDeVencimiento, venceRow, dato);
+  }
 };
 
 const recogerDatos = async () => {
   const responsive = await fetch(urlApi);
   const datos = await responsive.json();
   const datosTipoIngreso = datos.filter(dato => dato.tipo.includes("ingreso"));
+  const totales = document.querySelector(".totales").cloneNode(true);
+  totales.classList.remove("row-dummy");
+  const totalBase = totales.querySelector(".total-base");
+  const totalIva = totales.querySelector(".total-iva");
+  const totalTotal = totales.querySelector(".total-total");
+  // eslint-disable-next-line prefer-const
   for (const dato of datosTipoIngreso) {
     const filaClonada = clonarFila();
     const num = filaClonada.querySelector(".num");
@@ -35,7 +73,8 @@ const recogerDatos = async () => {
     estado.classList.remove("table-danger");
     vence.classList.remove("table-success");
     rellenarTabla(dato, num, fecha, concepto, base, iva, total, estado, vence);
-    tBody.prepend(filaClonada);
+    tBody.append(filaClonada);
   }
 };
+
 recogerDatos();
